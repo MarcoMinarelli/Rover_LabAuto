d3b424c6940aaedede9ede573ca7c0f6f98661bc
MarcoMinarelli <39798373+MarcoMinarelli@users.noreply.github.com>
2024-04-09 09:08:38 +0200

    Updated Logger node

--- code/utils/logger.cpp
+++ code/utils/logger.cpp
@@ -1,9 +1,7 @@
 
 // standard library includes 
 #include <vector>
-#include <string>
-#include <fstream>
-#include <sstream>
+
 #include <rclcpp/rclcpp.hpp>
 #include <rclcpp/qos.hpp>
 #include <geometry_msgs/msg/pose_stamped.hpp>
@@ -12,25 +10,48 @@
 #include "bbm_interfaces/msg/lineparams.hpp"
 
 
-using namespace std::chrono_literals;
+	
+#include <string>
+#include <iomanip>
+#include <fstream>
+#include <sstream>
+
+class WriteCSV {
+
+public:
+	std::string fileName;
+	std::ofstream csv_file;
+
+	WriteCSV(std::string filename) : fileName(filename) { this->csv_file.open(this->fileName); }
+
+
+	void writeData(std::vector<double> list) {
+		for (int i = 0; i < list.size()-1; ++i) {
+        		this->csv_file << std::setprecision(11) << list[i]<<",";
+		};
+		this->csv_file << std::setprecision(11) << list[list.size()]<<"\n";
+	}
+
+	void close_csv() { this->csv_file.close(); }
+};	
+	
 		
 class Logger : public rclcpp::Node{
 	private:
 		// subscriber 
 		rclcpp::Subscription<bbm_interfaces::msg::Lineparams>::SharedPtr lpSub;
 		rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr poseSub;
-		WriteCSV file_robot, file_params;
+		WriteCSV file_robot{"robot_pose.csv"};
+		WriteCSV file_params{"lines_params.csv"};
 		
 		
 	public:
 		Logger() : Node("logger") {
-			file_robot("robot_pose.csv");
-			file_params("lines_params.csv");
-			
+
 			rclcpp::QoS custom_qos(10);
 			auto sub_opt = rclcpp::SubscriptionOptions();
-        	lpSub = create_subscription<bbm_interfaces::msg::Lineparams>( "/bbm/line_params", custom_qos, std::bind(&Logger::lineParamsCallback, this, std::placeholders::_1), sub_opt);
-        	poseSub = create_subscription<geometry_msgs::msg::PoseStamped>("/zed/zed_node/pose", custom_qos, std::bind(&Logger::poseCallback, this, std::placeholders::_1), sub_opt);
+        		lpSub = create_subscription<bbm_interfaces::msg::Lineparams>( "/bbm/line_params", custom_qos, std::bind(&Logger::lineParamsCallback, this, std::placeholders::_1), sub_opt);
+        		poseSub = create_subscription<geometry_msgs::msg::PoseStamped>("/zed/zed_node/pose", custom_qos, std::bind(&Logger::poseCallback, this, std::placeholders::_1), sub_opt);
       			RCLCPP_INFO(this->get_logger(), "Logger_Node started");
 		}
 		
@@ -61,28 +82,4 @@ int main(int argc, char **argv) {
     	return 0;
 }
 
-class WriteCSV {
 
-public:
-	std::string fileName;
-	std::ofstream csv_file;
-
-	WriteCSV(std::string filename) :
-        	fileName(filename) { this->csv_file.open(this->fileName); }
-
-	void writeData(std::vector<double>);
-
-	void close_csv();
-};
-
-void WriteCSV::writeData(std::vector<double> list) {
-	for (int i = 0; i < list.size()-1; ++i) {
-        		this->csv_file << std::setprecision(11) << list[i]<<",";
-	};
-	this->csv_file << std::setprecision(11) << list[i]<<"\n";
-	//this->close_csv();
-};
-
-void WriteCSV::close_csv() {
-	this->csv_file.close();
-};
