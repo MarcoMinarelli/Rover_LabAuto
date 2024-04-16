f4c0f8bad7acbc73757996f2e5b07cd47a72a4b3
MarcoMinarelli <39798373+MarcoMinarelli@users.noreply.github.com>
2024-04-16 12:18:11 +0200

    Uploaded complementary filter and converter

--- code/utils/src/converter.cpp
+++ code/utils/src/converter.cpp
@@ -1,18 +1,22 @@
 // Standard library includes
 #include <vector>
 #include"PID.cpp"
+#include "complementaryFilter.cpp"
 
 //ROS includes
 #include <rclcpp/rclcpp.hpp>
 #include <rclcpp/qos.hpp>
 #include <geometry_msgs/msg/twist.hpp>
 #include <geometry_msgs/msg/point.hpp>
+#include <geometry_msgs/msg/pose_stamped.hpp>
+#include <sensor_msgs/msg/imu.hpp>
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2/LinearMath/Matrix3x3.h>
 
 #include <chrono>
 
 //DART includes
 #include "dart_interfaces/msg/commands.hpp"
-#include "dart_interfaces/msg/imu.hpp"
 
 using namespace std::chrono_literals;
 
@@ -25,32 +29,40 @@ class Converter : public rclcpp::Node{
 	private:
 	
 		PID p;
+		ComplementaryFilter cf;
 		
 		double desVel;
 		double desAng;
 		
 		double steering_current;
-		double estVel;// estimated linear velocity along x axis
-		
+		double estVel_acc;// estimated linear velocity along x axis
+		double estVel_pos;
+		double old_yaw;
+		std::vector<double> pose; //pose=[x_r, y_r] 
 		double deltaT = 10; //ms
 		
+		bool ok; 
 		rclcpp::TimerBase::SharedPtr timer;
 		
 		rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr twistSub;
 		
 
-		rclcpp::Subscription<dart_interfaces::msg::Imu>::SharedPtr imuSub;
+		rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr imuSub;
 		
 		rclcpp::Publisher<dart_interfaces::msg::Commands>::SharedPtr commands_pub;
 		
+		rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr poseSub;
+		
 	public:
 	
-		Converter() : Node("converter"), p(1,1,1,10,1,7) {
+		Converter() : Node("converter"), p(1,1,1,10,1,7), cf(4.74,62.6, 10), pose(2, 0) {
 			rclcpp::QoS custom_qos(10);
 			
 			desVel = 0;
 			desAng = 0;
-			estVel = 0;
+			estVel_acc = 0;
+			estVel_pos=0;
+			old_yaw=0;
 			steering_current=0;
 			
 			auto sub_opt = rclcpp::SubscriptionOptions();
@@ -59,8 +71,9 @@ class Converter : public rclcpp::Node{
         		twistSub = create_subscription<geometry_msgs::msg::Twist>( "/cmd_vel", custom_qos, std::bind(&Converter::twistCallback, this, std::placeholders::_1), sub_opt);
 
 
-        		imuSub = create_subscription<dart_interfaces::msg::Imu>( "dart/telemetry/imu", custom_qos, std::bind(&Converter::imuCallback, this, std::placeholders::_1), sub_opt);
+        		imuSub = create_subscription<sensor_msgs::msg::Imu>( "/zed/zed_node/imu/data", custom_qos, std::bind(&Converter::imuCallback, this, std::placeholders::_1), sub_opt);
 			timer = this->create_wall_timer(10ms, std::bind(&Converter::timerCallback, this));
+			poseSub = create_subscription<geometry_msgs::msg::PoseStamped>("/zed/zed_node/pose", custom_qos, std::bind(&Converter::poseCallback, this, std::placeholders::_1), sub_opt);
         	
         		commands_pub = this->create_publisher<dart_interfaces::msg::Commands>("dart/commands", 10);
       			RCLCPP_INFO(this->get_logger(), "Converter node started");
@@ -75,25 +88,50 @@ class Converter : public rclcpp::Node{
 		
 	
 	
-		void imuCallback(const dart_interfaces::msg::Imu::SharedPtr msg){
-			estVel += msg->acc.x * deltaT;
+		void imuCallback(const sensor_msgs::msg::Imu::SharedPtr msg){
+			estVel_acc += msg->linear_acceleration.x * deltaT;
 		}
 	
 		/** Callback for Timer. Computes from desired angular ad linear velocity the correct steering and throttle values **/
 		void timerCallback(){
-			double steering = steering_current + deltaT*desAng;
-			steering_current=steering;
-			double throttle = 0;
-			double error = desVel - estVel;
+			if(ok){
+				double steering = steering_current + deltaT*desAng;
+				steering_current=steering;
+				double throttle = 0;
+				double error = desVel - cf.update(estVel_pos, estVel_acc);
+				
+				throttle = p.compute(error);
+				
+				
+				//RCLCPP_INFO(this->get_logger(), "Throttle calcolato", throttle);
+				auto msg = new dart_interfaces::msg::Commands();
+			 	msg->header.stamp  = now();
+			 	msg->steering.data = steering;
+			 	msg->throttle.data = throttle;
+			 	commands_pub->publish(*msg);
+		 	}
+		}
+		
+		void poseCallback(const geometry_msgs::msg::PoseStamped::SharedPtr msg){
+			tf2::Quaternion q(msg->pose.orientation.x, msg->pose.orientation.y, msg->pose.orientation.z,  msg->pose.orientation.w);
+
+			// 3x3 Rotation matrix from quaternion
+			tf2::Matrix3x3 m(q);
+
+			// Roll Pitch and Yaw from rotation matrix
+			double roll, pitch, yaw;
+			m.getRPY(roll, pitch, yaw);
+			double x_dot=(msg->pose.position.x-pose[0])/deltaT;
+			double psi_dot=(yaw-old_yaw)/deltaT;
+			estVel_pos=x_dot-psi_dot*msg->pose.position.y;
 			
-			throttle = p.compute(error);
+
 			
-			auto msg = new dart_interfaces::msg::Commands();
-		 	msg->header.stamp  = now();
-		 	msg->steering.data = steering;
-		 	msg->throttle.data = throttle;
-		 	commands_pub->publish(*msg);
-			
+		    	pose[0] =  msg->pose.position.x;
+		    	pose[1] =  msg->pose.position.y;
+			old_yaw=yaw;
+
+		 	ok = true;
 		}
 			
 		
