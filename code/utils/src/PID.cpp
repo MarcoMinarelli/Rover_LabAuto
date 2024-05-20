630045084eab28ad1c2965ff24f948641fe05d68
MarcoMinarelli <39798373+MarcoMinarelli@users.noreply.github.com>
2024-05-20 12:25:22 +0200

    Updated PID

--- code/utils/src/PID.cpp
+++ code/utils/src/PID.cpp
@@ -1,4 +1,6 @@
 #include <cmath>
+#include <rclcpp/rclcpp.hpp>
+#include "writecsv.cpp"
 
 /**	Class that implements a discrete time PID with Back Calculation and Tracking
 **/
@@ -11,46 +13,54 @@ class PID{
 		double kd;
 		
 		double deltaT;
-		double Tt;
+
 		double sum;
 		double last_err;
 		double U_max;
 		double U_min;
 		
-		double sum_bct; 
-		
+
+		WriteCSV file{"pid.csv"};
 		
 	public:
 	
-		PID(double p, double i, double d, double deltaT, double Tt, double U_max,double U_min): kp(p), ki(i), kd(d), deltaT(deltaT), Tt(Tt), U_max(U_max), U_min(U_min){
+		PID(double p, double i, double d, double deltaT, double U_max,double U_min): kp(p), ki(i), kd(d), deltaT(deltaT), U_max(U_max), U_min(U_min){
 			sum=0;
 			last_err=0;
-			sum_bct = 0;
-		}
-		
-		PID(double p, double i, double d, double deltaT, double U_max, double U_min): kp(p), ki(i), kd(d), deltaT(deltaT), Tt(Tt), U_max(U_max), U_min(U_min){
-			sum=0;
-			last_err=0;
-			sum_bct = 0;
-			Tt = sqrt( kd/ki ); // automatic calculation of Tt as sqrt(Td * Ti)
 		}
 		
 	
 		double compute(double e){
+			//RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "error PID %f ", e);
 			double ret=kp*e;
+			
+
 			
 			double new_sum;
 			new_sum=sum+e;
 			ret=ret+new_sum*ki*deltaT;
 			
-			ret=ret+(e-last_err)/deltaT;
-			last_err=e;
+
 			
-			if(ret>U_max){
-				ret=U_max;
-			}
+			ret=ret+ kd * (e-last_err)/deltaT;
+			
+
+			std::vector<double> v(5,0);
+                                        v[0] = kp*e;
+                                        v[1] = new_sum*ki*deltaT;
+                                        v[2] = kd * (e-last_err)/deltaT;
+                                        v[3] = ret;
+                                        v[4] = e;
+                                        file.writeData (v);
+
+			last_err=e;
+			//RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "risultato prop %f risultato int %f risultato der %f risultato PID %f prima sat", kp*e, new_sum*ki*deltaT, kd * (e-last_err)/deltaT,   renew_sum*ki*deltaTt);
+			
+		
+
+			if(ret>U_max) ret=U_max;
 			else if (ret<U_min) ret=U_min;
-			/* else*/  sum=new_sum;
+			else  sum=new_sum;
 			return ret;
 		}
 		
